347b380323fb7c0657420e0ef80522c7ed4f18ca
Colin <cblokkeeltech@gmail.com>
2023-02-26 14:30:34 +0100

    Initial commit

---
+++ apps/moldavite-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'moldavite-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/moldavite-api',
+};
